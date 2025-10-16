c119b075a6d796a9d6cfb0eb99446f5837f1b7e4
kuailefengnan2024 <han552401231@gmail.com>
2025-10-16 18:38:32 +0800

    save

--- __init__.py
+++ __init__.py
@@ -4,9 +4,12 @@ from .nodes.my_first_nodes import NODE_CLASS_MAPPINGS as text_nodes, NODE_DISPLA
 # 从新的图像节点文件中导入
 from .nodes.image_nodes import NODE_CLASS_MAPPINGS as image_nodes, NODE_DISPLAY_NAME_MAPPINGS as image_nodes_display
 
+# 从图像选择节点文件中导入
+from .nodes.image_selection_nodes import NODE_CLASS_MAPPINGS as selection_nodes, NODE_DISPLAY_NAME_MAPPINGS as selection_nodes_display
+
 # 将所有导入的节点合并到一个字典中
-NODE_CLASS_MAPPINGS = {**text_nodes, **image_nodes}
-NODE_DISPLAY_NAME_MAPPINGS = {**text_nodes_display, **image_nodes_display}
+NODE_CLASS_MAPPINGS = {**text_nodes, **image_nodes, **selection_nodes}
+NODE_DISPLAY_NAME_MAPPINGS = {**text_nodes_display, **image_nodes_display, **selection_nodes_display}
 
 # 将合并后的字典导出给 ComfyUI
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
